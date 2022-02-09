42d5bd3a011cf01154f7415fc644246a33c1e5ac
gabGodiskova <gabgodiskova@gmail.com>
2022-02-09 14:29:24 +0000

    Deploying to docs from @ books-are-next/wilde-lady-fuckingham@0994430a574da95518e8b2abfdae133de481f220 🚀

--- epub2nb-output/content/_book.md
+++ epub2nb-output/content/_book.md
@@ -1,9 +1,33 @@
 ---
 outputs: meta
 slug: book
-languageCode: en
-readingOrder: []
-tocBase: null
+languageCode: cs
+meta:
+  title: Lady Fuckingham
+  identifier: urn:uuid:296c0919-9963-415a-a386-6c26c077b931
+  author: Oscar Wilde
+  publisher: Městská knihovna v Praze
+  modified: '2022-02-09T14:15:03.140Z'
+  languageCode: cs
+  publisherShort: mkp
+  edition: beletrie
+readingOrder:
+  - lady_fuckingham_005.html
+  - lady_fuckingham_006.html
+  - lady_fuckingham_007.html
+  - lady_fuckingham_008.html
+  - lady_fuckingham_009.html
+  - lady_fuckingham_010.html
+  - lady_fuckingham_011.html
+  - lady_fuckingham_012.html
+  - lady_fuckingham_013.html
+  - lady_fuckingham_014.html
+  - lady_fuckingham_015.html
+  - colophon.html
+tocBase:
+  - id: section-1
+    children: []
+    isSection: true
 static:
   - style
   - scripts
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-0994430';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./lady_fuckingham_005.html","./lady_fuckingham_006.html","./lady_fuckingham_007.html","./lady_fuckingham_008.html","./lady_fuckingham_009.html","./lady_fuckingham_010.html","./lady_fuckingham_011.html","./lady_fuckingham_012.html","./lady_fuckingham_013.html","./lady_fuckingham_014.html","./lady_fuckingham_015.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/obalka_lady_fuckingham.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
